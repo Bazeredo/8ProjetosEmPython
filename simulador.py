79104889df3b4aae68b8d8304267413cb72f3320
Bruno <brunosilvaazeredo@gmail.com>
2021-01-26 18:54:15 -0300

    Simulador

--- simulador.py
+++ simulador.py
@@ -18,20 +18,16 @@ class SimuladorDeDado:
         #ler os valores da tela
         self.eventos, self.valores = self.janela.Read()
         #fazer alguma coisa com esses valores
-       
-        while True:
-            try:
-                     if self.eventos == 'sim' or self.eventos == 's' :
-                            self.GerarValorDoDado()
-                     elif self.eventos == 'não' or self.eventos =='n':
-                            print('Agradecemos a participação!')
-                     else:
-                            print('Favor digitar sim ou não')
-            except:
-                print('Ocorreu um erro ao receber sua resposta')
+        try:
+            if self.eventos == 'sim' or self.eventos == 's' :
+                self.GerarValorDoDado()
+            elif self.eventos == 'não' or self.eventos =='n':
+                print('Agradecemos a participação!')
+            else:
+                print('Favor digitar sim ou não')
+        except:
+            print('Ocorreu um erro ao receber sua resposta')
         
-
-    
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo))
 
